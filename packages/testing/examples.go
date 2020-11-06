d34f236f69267ffca6cd5781be536190ac02015f
chriswilliams1977 <chriswilliams1977@gmail.com>
2020-11-06 09:26:45 +0100

    test examples

--- packages/testing/examples.go
+++ packages/testing/examples.go
@@ -7,13 +7,19 @@ import "strings"
 //produces state-of-the-art
 //if we run the below it works ok for two words but how do we test this
 func JoinWithCommas(phrases []string) string{
-	//strings.join takes a slice of strings and a string to join
-	//:len(phrases)-1] gets all strings upto last string and joins them with comma in between
-	result := strings.Join(phrases[:len(phrases)-1],", ")
-	//add and
-	result += ", and "
-	//add the last string in slice
-	result += phrases[len(phrases)-1]
-	//returns string with items joined
-	return result
+	if len(phrases) == 1 {
+		return phrases[0]
+	} else if len(phrases) ==2 {
+		return phrases[0] + " and " + phrases[1]
+	} else {
+		//strings.join takes a slice of strings and a string to join
+		//:len(phrases)-1] gets all strings upto last string and joins them with comma in between
+		result := strings.Join(phrases[:len(phrases)-1],", ")
+		//add and
+		result += " and "
+		//add the last string in slice
+		result += phrases[len(phrases)-1]
+		//returns string with items joined
+		return result
+	}
 }
--- packages/testing/examples_test.go
+++ packages/testing/examples_test.go
@@ -1,8 +1,46 @@
 package testing
 
 import (
+	"fmt"
 	"testing"
 )
+
+//struct to hold data for tests
+//contains input and output for each test
+type testData struct{
+	list []string
+	want string
+}
+
+//you can use a struct to hold the inout and output of each test
+func TestJoinWithCommas(t *testing.T){
+	//create a string of testData
+	tests := []testData{
+		//you can omit testData as go know the type from parent
+		{list: []string{"apple"}, want: "apple"},
+		{list: []string{"apple","orange"}, want: "apple and orange"},
+		{list: []string{"apple","orange","pear"}, want: "apple, orange and pear"},
+	}
+	//for each test
+	for _,test := range tests{
+		//get the string from the list
+		got := JoinWithCommas(test.list)
+		//test if what you got is what you want
+		if got != test.want {
+			t.Errorf(errorString(test.list,got,test.want))
+		}
+	}
+}
+
+/*
+func TestOneElement(t *testing.T){
+	list := []string{"apple"}
+	want := "apple"
+	got := JoinWithCommas(list)
+	if got != want {
+		t.Errorf(errorString(list,got,want))
+	}
+}
 
 //tests the expected output from JoinWithCommas with two elements
 func TestTwoElements(t *testing.T){
@@ -10,7 +48,7 @@ func TestTwoElements(t *testing.T){
 	want := "apple and orange"
 	got := JoinWithCommas(list)
 	if got != want {
-		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list,got,want)
+		t.Errorf(errorString(list,got,want))
 	}
 }
 
@@ -20,6 +58,14 @@ func TestThreeElements(t *testing.T){
 	want := "apple, orange and pear"
 	got := JoinWithCommas(list)
 	if got != want {
-		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list,got,want)
+		t.Errorf(errorString(list,got,want))
 	}
+}
+*/
+
+
+//helper functions can be created to help refactor code
+//example this one format error string used across multiple tests
+func errorString(list []string, got string, want string) string{
+	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list,got,want)
 }
